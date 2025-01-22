c472992212d50414e2beac491a6a1d3684aab9f1
Andersen Ball <andersen.ball@gmail.com>
2025-01-22 18:28:52 -0500

    implemented nation system

---
+++ Assets/MapMode/Scripts/DataTypes/Nation/Nation.cs
@@ -0,0 +1,10 @@
+public enum Nation
+{
+    Britain,
+    Spain,
+    Netherlands,
+    France,
+    RepublicOfPirates, // Nassau-based pirate group
+    BarbaryCorsairs,   // North African pirates
+    BrethrenOfTheCoast // Caribbean-based pirate group
+}
