36edfb849d5a9478d0120d8c6c1bd3434cd77111
Andersen Ball <andersen.ball@gmail.com>
2025-07-20 22:53:11 -0400

    split boat controls

---
+++ Assets/Combat/Scripts/PlayerScripts/PlayerBoatControls.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+namespace Combat.Scripts.PlayerScripts
+{
+    public class PlayerBoatControls: MonoBehaviour
+    {
+        
+        public BoatControls boatControl { get; set; }
+
+        public bool isDriving { get; set; }= false;
+        
+        //boat controls variables
+        float sideways = 0;
+        float throtal = 0;
+    
+        void Start()
+        {
+            boatControl = gameObject.GetComponentInParent<BoatControls>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (isDriving) {
+                sideways = (Input.GetKey(KeyCode.A) ? -1f : 0f) +
+                           (Input.GetKey(KeyCode.D) ? 1f : 0f);
+                boatControl.SetTurn(sideways);
+                if (Input.GetKeyDown(KeyCode.W)) {
+                    throtal = throtal + .1f;
+                    throtal = Mathf.Clamp(throtal, -1, 1);
+                    boatControl.SetForward(throtal);
+                }
+                else if (Input.GetKeyDown(KeyCode.S)) {
+                    throtal = throtal - .1f;
+                    throtal = Mathf.Clamp(throtal, -1, 1);
+                    boatControl.SetForward(throtal);
+                }
+                        
+
+            }
+        }
+        
+        
+        
+    }
+}
--- Assets/Combat/Scripts/PlayerScripts/PlayerMovementOnBoat.cs
+++ Assets/Combat/Scripts/PlayerScripts/PlayerMovementOnBoat.cs
@@ -6,54 +6,16 @@ public class PlayerMovementOnBoat : MonoBehaviour
 {
 
     public CharacterPositionInterface characterPositionInterface;
-    public BoatControls boatControl;
 
-    public bool isFirstPerson = true;
-
-    //boat controls variables
-    float sideways = 0;
-    float throtal = 0;
     
-    void Start()
-    {
-        boatControl = gameObject.GetComponentInParent<BoatControls>();
-    }
-
     // Update is called once per frame
     void Update()
     {
-        if (isFirstPerson == false) {
-            sideways = (Input.GetKey(KeyCode.A) ? -1f : 0f) +
-                        (Input.GetKey(KeyCode.D) ? 1f : 0f);
-            boatControl.SetTurn(sideways);
-            if (Input.GetKeyDown(KeyCode.W)) {
-                throtal = throtal + .1f;
-                throtal = Mathf.Clamp(throtal, -1, 1);
-                boatControl.SetForward(throtal);
-            }
-            else if (Input.GetKeyDown(KeyCode.S)) {
-                throtal = throtal - .1f;
-                throtal = Mathf.Clamp(throtal, -1, 1);
-                boatControl.SetForward(throtal);
-            }
-                        
-
-        }
+        
         UpdateLocalPosition();
     }
     
-    public void SetIsActive(bool active)
-    {
-        isFirstPerson = active;
-        characterPositionInterface.SetFirstPerControllerActive(active);
-    }
-    public void ActiveBoatControls(BoatControls b)
-    {
-        boatControl = b;
-        isFirstPerson = false;
-        characterPositionInterface.SetFirstPerControllerActive(false);
-    }
-
+ 
     //moves the object to the location of the nav mesh object, simulating movement.
     public void UpdateLocalPosition()
     {
