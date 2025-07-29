9aca3fd4ed0014ec947fbc9b2e8937b30e273189
AndersenBall <andersen.ball@gmail.com>
2025-07-29 11:51:15 -0400

    base dialog system

---
+++ Assets/GerneralScripts/DialogSystem/DialogNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GerneralScripts.DialogSystem
+{
+    public class DialogNode
+    {
+        public string Id { get; set; }  // Unique identifier for linking
+        public string NpcLine { get; set; }
+
+        // Optional condition to determine if this node should be shown
+        public Func<bool>? Prerequisite { get; set; }
+
+        // Optional action to run when this node is entered (e.g., give gold)
+        public Action? OnEnter { get; set; }
+
+        public List<DialogChoice> Choices { get; set; } = new();
+    }
+}
